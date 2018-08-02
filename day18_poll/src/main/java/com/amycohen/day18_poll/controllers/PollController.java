5d34e520bb1d58a7e49bd5038682841b02c456e6
Amy Cohen <anbcohen@gmail.com>
2018-08-02 09:51:52 -0700

    got proof of life from Steve's demo code and now am changing things to work for the project

---
+++ day18_poll/src/main/java/com/amycohen/day18_poll/controllers/PollController.java
@@ -0,0 +1,64 @@
+package com.amycohen.day18_poll.controllers;
+
+import com.amycohen.day18_poll.model.Poll;
+import com.amycohen.day18_poll.repositories.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/questions")
+public class PollController {
+    @Autowired
+    PollRepository pollRepository;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public List<Poll> getAll() {
+        List<Poll> questions = pollRepository.findAll();
+        Collections.sort(questions);
+        return questions;
+    }
+
+    @PostMapping("/")
+    @ResponseBody
+    public Poll create(
+         @RequestParam String questions,
+         @RequestParam int upvotes,
+         @RequestParam int downvotes
+    ) {
+        Poll pollQuestion = new Poll(questions, upvotes, downvotes);
+        pollQuestion = pollRepository.save(pollQuestion);
+        return pollQuestion;
+    }
+
+    @GetMapping("/{id}/upvote")
+    public String upvote(
+            @PathVariable("id") long id
+    ) {
+        Optional optional = pollRepository.findById(id);
+        Poll pollQuestion = (Poll) optional.get();
+        if (pollQuestion != null) {
+            pollQuestion.upvotes++;
+            pollRepository.save(pollQuestion);
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/downvote")
+    public String downvote(
+            @PathVariable("id") long id
+    ) {
+        Optional optional = pollRepository.findById(id);
+        Poll pollQuestion = (Poll) optional.get();
+        if (pollQuestion != null) {
+            pollQuestion.downvotes++;
+            pollRepository.save(pollQuestion);
+        }
+        return "redirect:/";
+    }
+}
